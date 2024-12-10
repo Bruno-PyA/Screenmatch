d049a8883f32b436f34530bc9b02c0d0dee0d2ec
Bruno Nogueira <brunodenogueira@gmail.com>
2024-12-10 16:42:10 -0300

    First Project-First Commit

---
+++ JavApisScreenMatch/src/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,82 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        List<Filme> listaDeFilmes = criarFilmesESeries();
+
+        exibirDetalhesDeFilmes(listaDeFilmes);
+
+        calcularTempoTotal(listaDeFilmes);
+
+        aplicarFiltroDeRecomendacao(listaDeFilmes);
+
+        System.out.println("O programa finalizou corretamente!");
+    }
+
+    private static @NotNull List<Filme> criarFilmesESeries() {
+        List<Filme> filmes = new ArrayList<>();
+
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        filmes.add(meuFilme);
+
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.setDuracaoEmMinutos(200);
+        filmes.add(outroFilme);
+
+        Filme filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.avalia(10);
+        filmes.add(filmeDoPaulo);
+
+        Serie lost = new Serie("Lost", 2000);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+
+        return filmes;
+    }
+
+    private static void exibirDetalhesDeFilmes(@NotNull List<Filme> filmes) {
+        System.out.println("Exibindo detalhes dos filmes:");
+        for (Filme filme : filmes) {
+            filme.exibeFichaTecnica();
+            System.out.println("Média de avaliações: " + filme.pegaMedia());
+        }
+    }
+
+    private static void calcularTempoTotal(@org.jetbrains.annotations.NotNull List<Filme> filmes) {
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        filmes.forEach(calculadora::inclui);
+
+        System.out.println("Tempo total de reprodução: " + calculadora.getTempoTotal() + " minutos");
+    }
+
+    private static void aplicarFiltroDeRecomendacao(@NotNull List<Filme> filmes) {
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+
+        System.out.println("Aplicando filtros de recomendação:");
+        for (Filme filme : filmes) {
+            filtro.filtra(filme);
+        }
+    }
+}
---
+++ JavApisScreenMatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,65 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        // Criação de objetos Filme e definição de avaliações
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);  // Filme "O Poderoso Chefão"
+        meuFilme.avalia(9);  // Avaliação do filme
+        Filme outroFilme = new Filme("Avatar", 2023);  // Filme "Avatar"
+        outroFilme.avalia(6);  // Avaliação do filme
+        var filmeDoPaulo = new Filme("Dogville", 2003);  // Filme "Dogville"
+        filmeDoPaulo.avalia(10);  // Avaliação do filme
+        Serie lost = new Serie("Lost", 2000);  // Série "Lost"
+
+        // Atribuindo um filme à variável do tipo Filme
+        Filme f1 = filmeDoPaulo;
+
+        // Criando uma lista de Titulo (pode armazenar tanto Filme quanto Serie)
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);  // Adicionando filme à lista
+        lista.add(meuFilme);  // Adicionando filme à lista
+        lista.add(outroFilme);  // Adicionando filme à lista
+        lista.add(lost);  // Adicionando série à lista
+
+        // Iterando sobre a lista de títulos e exibindo o nome
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());  // Imprime o nome do título
+            // Verifica se o título é do tipo Filme e se a classificação é maior que 2
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());  // Imprime a classificação do filme
+            }
+        }
+
+        // Criando uma lista de busca por artista (LinkedList)
+        List<String> buscaPorArtista = new LinkedList<>();
+        buscaPorArtista.add("Adam Sandler");  // Adicionando artistas à lista
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+        System.out.println(buscaPorArtista);  // Exibindo a lista de artistas
+
+        // Ordenando a lista de artistas alfabeticamente
+        Collections.sort(buscaPorArtista);  // Ordenação em ordem alfabética
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);  // Exibindo a lista ordenada
+
+        // Exibindo a lista de títulos antes da ordenação
+        System.out.println("Lista de titulos ordenados");
+        Collections.sort(lista);  // Ordenando a lista de títulos de acordo com a ordem natural de Titulo (pelo nome)
+        System.out.println(lista);  // Exibindo a lista ordenada
+
+        // Ordenando a lista de títulos por ano de lançamento
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));  // Ordenando pela data de lançamento
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);  // Exibindo a lista ordenada por ano de lançamento
+    }
+}
